bae14fc01086d4915b5b365fdfd827b5ef8e854e
Ross MacArthur <ross@macarthur.io>
2020-10-01 17:29:12 +0200

    Add `ggpf` Zsh command

--- src/zsh/plugins/aliases.plugin.zsh
+++ src/zsh/plugins/aliases.plugin.zsh
@@ -15,3 +15,14 @@ alias l='ls -lah'
 alias la='ls -lAh'
 alias lh="ls -a | egrep '^\.'"
 alias ll='ls -lh'
+
+# Copy of `ggp` in Oh My Zsh Git plugin to push to my usual fork name.
+function ggpf() {
+  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
+    git push rossmacarthur "${*}"
+  else
+    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
+    git push rossmacarthur "${b:=$1}"
+  fi
+}
+compdef _git ggp=git-checkout
