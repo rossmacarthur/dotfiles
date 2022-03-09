be4f14bf96d28e94d282f1959a2f364eb20f3d9e
Ross MacArthur <ross@macarthur.io>
2022-03-09 10:21:20 +0200

    bin: Update gbDs script

--- src/bin/gbDs.sh
+++ src/bin/gbDs.sh
@@ -1,15 +1,27 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # From https://blog.takanabe.tokyo/en/2020/04/remove-squash-merged-local-git-branches/
 
-set -euo pipefail
+set -e
+
+git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return
+    fi
+  done
+  echo master
+}
 
 echo "Removing out-dated local squash-merged branches..."
-git fetch origin -p &>/dev/null
 git for-each-ref refs/heads/ "--format=%(refname:short)" |
   while read -r branch; do
-    ancestor=$(git merge-base origin/master $branch)
+    main=$(git_main_branch)
+    ancestor=$(git merge-base origin/$main $branch)
     temp=$(git commit-tree $(git rev-parse $branch^{tree}) -p $ancestor -m _)
-    [[ $(git cherry origin/master $temp) == "-"* ]] && git branch -D $branch
+    [[ $(git cherry origin/$main $temp) == "-"* ]] && git branch -D $branch
   done
 echo "Finished!"
