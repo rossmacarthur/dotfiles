58174ef8beb20d35264d08eb92845df1355dfa7c
Ross MacArthur <ross@macarthur.io>
2025-11-09 19:06:54 +0200

    zsh: Do not override SSH agent if already set
    This is useful when SSHing into another machine that uses this plugin.

--- src/zsh/plugins/gpg-agent.plugin.zsh
+++ src/zsh/plugins/gpg-agent.plugin.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+if [[ -n "$SSH_AUTH_SOCK" && "$SSH_AUTH_SOCK" != /private/tmp/com.apple.launchd* ]]; then
+  return &>/dev/null || exit 0
+fi
+
 load_gpg_agent() {
   unset SSH_AGENT_PID
   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
